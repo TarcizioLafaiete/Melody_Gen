5489524459c9b27820ce891c2bcc71d5b7b1f06f
TarcizioLafiaete <tarcizio-augusto@hotmail.com>
2025-01-13 00:37:44 -0300

    Separando os dados coletados por musica

--- dataset.py
+++ dataset.py
@@ -5,12 +5,13 @@ from music21 import converter, instrument, note, chord
 
 def generate_notesArray():
 
-    notes = []
-    duration = []
+    music_num = 0
 
     for file in glob.glob("midi_songs/*.midi"):
         midi = converter.parse(file)
         parse = None
+        notes = []
+        duration = []
 
         parts = instrument.partitionByInstrument(midi)
 
@@ -34,13 +35,12 @@ def generate_notesArray():
                 symb = '.'.join(str(n) for n in element.normalOrder)
             elif isinstance(element,note.Rest):
                 symb = "Rest"
-            notes.append((symb,str(element.quarterLength))) 
-    print(f"Tamanho do array: {len(notes)} - variedade: {len(set(notes))}")
-    print(f"Tamanho do array: {len(duration)} - variedade: {len(set(duration))}")
+            notes.append((symb,str(element.quarterLength)))
 
-    data = {"notes": notes, "durations": duration}
-    with open("notes_durations.json", "w") as json_file:
-        json.dump(data, json_file, indent=4)
+        data = {f"music_{music_num}" : {"notes": notes, "durations": duration}}
+        with open("notes_durations.json", "+a") as json_file:
+            json.dump(data, json_file, indent=1)
+        music_num += 1
 
 
 generate_notesArray()
