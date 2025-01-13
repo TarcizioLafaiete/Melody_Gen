f23b96f22b224f26507736cfda4ae9272814218f
TarcizioLafiaete <tarcizio-augusto@hotmail.com>
2025-01-12 23:43:08 -0300

    Desenvolvendo script de coleta de dados musicais

--- dataset.py
+++ dataset.py
@@ -6,6 +6,7 @@ from music21 import converter, instrument, note, chord
 def generate_notesArray():
 
     notes = []
+    duration = []
 
     for file in glob.glob("midi_songs/*.midi"):
         midi = converter.parse(file)
@@ -18,9 +19,28 @@ def generate_notesArray():
         else:
             parse = midi.flat.notes
 
+        last_offset = 0
+        symb = ""
+
         for element in parse:
+
+            time_diff = element.offset - last_offset
+            last_offset = element.offset
+            duration.append(float(time_diff))
+
             if isinstance(element,note.Note):
-                notes.append((str(element.pitch),str(element.quarterLength)))
+                symb = str(element.pitch)
             elif isinstance(element,chord.Chord):
-                notes.append((('.'.join(str(n) for n in element.normalOrder)),element.quarterLength))
-                
+                symb = '.'.join(str(n) for n in element.normalOrder)
+            elif isinstance(element,note.Rest):
+                symb = "Rest"
+            notes.append((symb,str(element.quarterLength))) 
+    print(f"Tamanho do array: {len(notes)} - variedade: {len(set(notes))}")
+    print(f"Tamanho do array: {len(duration)} - variedade: {len(set(duration))}")
+
+    data = {"notes": notes, "durations": duration}
+    with open("notes_durations.json", "w") as json_file:
+        json.dump(data, json_file, indent=4)
+
+
+generate_notesArray()
--- experiments/exp.py
+++ experiments/exp.py
@@ -3,30 +3,15 @@ import glob
 
 from music21 import converter, instrument, note, chord
 
-
 notes = []
+duration = []
+with open("notes_durations.json",'r') as file:
+    settings = json.load(file)
+    notes = settings['notes']
+    duration = settings['durations']
 
-file = "beethoven_opus10_3.mid"
-midi = converter.parse(file)
-parse = None
+    for a in notes:
+        print(a)
 
-parts = instrument.partitionByInstrument(midi)
-
-# parts.show("text")
-
-if parts:
-    parse = parts.parts[1].recurse()
-else:
-    parse = midi.flat.notes
-
-# print(parse)
-
-for element in parse:
-    if isinstance(element,note.Note):
-        notes.append((str(element.pitch),str(element.quarterLength)))
-    elif isinstance(element,chord.Chord):
-        notes.append((('.'.join(str(n) for n in element.normalOrder)),str(element.quarterLength)))
-    elif isinstance(element,note.Rest):
-        notes.append(("Rest",str(element.quarterLength)))
-
-print(notes)
+    for b in duration:
+        print(b)
