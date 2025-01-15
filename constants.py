4dad470eba5a9267fa9404ad57f4eaa5be1abdff
TarcizioLafiaete <tarcizio-augusto@hotmail.com>
2025-01-15 00:34:18 -0300

    refactor: Modificando o formato de algumas constantes

--- constants.py
+++ constants.py
@@ -1,10 +1,11 @@
 #Arquivo json utilizando atualmente
 JSON_FILE = "files/notes_and_offsets_v4.json"
-SELECTED_JSON_FILE = "files/selected_musics.json"
+TRAIN_FILE = "files/train.json"
+VALIDATION_FILE = "files/validation.json"
 
 #Constantes relacionadas com o tamanho do conjunto de dados utilizados no treinamento
 MUSIC_MIN_INDEX = 0
-MUSIC_MAX_INDEX = 200
+MUSIC_MAX_INDEX = 20
 TRAIN_PERCENTAGE = 0.8
 
 #Arquivos de maps e reversos
@@ -13,7 +14,7 @@ OFFSET_LABEL = "files/offset_labels.json"
 
 #Constantes de treinamento e do modelo
 SEQUENCE_LEN = 100
-BATCH_SIZE = 32
+BATCH_SIZE = 2
 EPOCHS = 200
-LOSS = ""
-OPTIMIZER = ""
+LOSS = ["categorical_crossentropy", "categorical_crossentropy"]
+OPTIMIZER = "adam"
